e714d5f96c4d060c00fe686824bd59ba150fa2d7
Maxim Filimonov <tpaktopsp@gmail.com>
2018-03-24 18:57:59 +0800

    done with part4

--- src/part_4.test.ts
+++ src/part_4.test.ts
@@ -1,6 +1,50 @@
-import("./part_4");
-describe("Part 4 with TS", () => {
-  it("works with Typescript!", () => {
-    expect(true).toEqual(true);
+import {
+  alwaysDrivingAge,
+  forever21,
+  IPerson,
+  isEligibleToVote,
+  someMagicMath,
+  square,
+  water
+} from "./part_4";
+
+describe("Part 4", () => {
+  it("#square makes things 3 times bigger", () => {
+    expect(square(3)).toEqual(9);
+  });
+  it("#someMagicMath does weird things", () => {
+    expect(someMagicMath(2, 3)).toEqual((2 * 3 + 1) * 7);
+  });
+  it("#isEligibleToVote filters youngsters", () => {
+    let people: IPerson[];
+    people = [
+      { birthCountry: "Indonesia", age: 15 },
+      { birthCountry: "Indonesia", age: 25 },
+      { birthCountry: "Indonesia", age: 16 }
+    ];
+    expect(people.filter(isEligibleToVote).length).toEqual(1);
+  });
+  it("#isEligibleToVote filters foreigners", () => {
+    let people: IPerson[];
+    people = [
+      { birthCountry: "Australia", age: 35 },
+      { birthCountry: "Russia", age: 25 },
+      { birthCountry: "Indonesia", age: 20 }
+    ];
+    expect(people.filter(isEligibleToVote).length).toEqual(1);
+  });
+  it("#forever21 returns 21 even if you are 30", () => {
+    expect(forever21(30)).toEqual(21);
+    expect(forever21(15)).toEqual(16);
+  });
+  it("#alwaysDrivingAge", () => {
+    expect(alwaysDrivingAge(30)).toEqual(30);
+    expect(alwaysDrivingAge(12)).toEqual(16);
+  });
+
+  it("#water descriptions are correct", () => {
+    expect(water(0)).toContain("freezes");
+    expect(water(100)).toContain("boils");
+    expect(water(5)).toContain("nothing");
   });
 });
--- src/part_4.ts
+++ src/part_4.ts
@@ -1,1 +1,54 @@
-export default "TEST";
+import {
+  __,
+  always,
+  both,
+  compose,
+  cond,
+  defaultTo,
+  either,
+  equals,
+  gte,
+  identity,
+  ifElse,
+  inc,
+  lt,
+  lte,
+  multiply,
+  T,
+  unless,
+  when
+} from "ramda";
+export interface IPerson {
+  birthCountry: string;
+  naturalizationDate?: Date;
+  age: number;
+}
+// tslint:disable-next-line:no-console
+const log = x => console.log(x) && x;
+
+export const square = x => multiply(x, x);
+
+export const someMagicMath: (
+  base: number,
+  multiplier: number
+) => number = compose(square, inc, multiply);
+
+export const OUR_COUNTRY = defaultTo("Indonesia", undefined);
+const wasBornInCountry = (person: IPerson) =>
+  equals(person.birthCountry, OUR_COUNTRY);
+
+const wasNaturalized = (person: IPerson) => Boolean(person.naturalizationDate);
+const isOver18 = (person: IPerson) => gte(person.age, 18);
+const isCitizen = either(wasNaturalized, wasBornInCountry);
+
+export const isEligibleToVote = both(isCitizen, isOver18);
+
+export const forever21 = ifElse(gte(__, 21), always(21), inc);
+
+export const alwaysDrivingAge = unless(gte(__, 16), always(16));
+
+export const water = cond([
+  [equals(0), always("water freezes at 0C")],
+  [equals(100), always("water boils at 100")],
+  [T, temp => `nothing special happens at ${temp}`]
+]);
